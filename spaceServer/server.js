f9851214496a3527825850b08aed75e298a6cec3
unknown <alexanderson812@gmail.com>
2026-02-28 08:41:42 -0500

    server fixes

---
+++ spaceServer/server.js
@@ -0,0 +1,77 @@
+//node server.js
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const WebSocket = require("ws");
+
+const ROOT = path.join(__dirname, "..");
+
+let gameState = {
+  ships: [],
+  showAllBands: false,
+  showWepBands: false
+};
+
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url === "/" ? "/index.html" : req.url;
+  filePath = path.join(ROOT, filePath);
+
+  const ext = path.extname(filePath);
+
+  const contentTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".gif": "image/gif"
+  };
+
+  const contentType = contentTypes[ext] || "application/octet-stream";
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      return res.end("Not found");
+    }
+
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  });
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", ws => {
+  console.log("Connected")
+  ws.send(JSON.stringify({
+    type: "init",
+    state: gameState
+  }));
+
+  ws.on("message", msg => {
+    const data = JSON.parse(msg);
+
+    if (data.type === "stateUpdate") {
+      gameState = data.state;
+
+      // Broadcast to everyone
+      wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({
+            type: "stateUpdate",
+            state: gameState
+          }));
+        }
+      });
+    }
+  });
+});
+
+/* ===============================
+   START SERVER
+================================ */
+server.listen(8080, () => {
+  console.log("Server running at http://localhost:8080");
+});
